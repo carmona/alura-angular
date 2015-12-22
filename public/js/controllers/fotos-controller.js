69610b0b6ba38e6cadc1000da5bbfae016ede7ad
carmona <filipecarmona@outlook.com>
2015-12-21 23:15:04 -0200

    aula 2:

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,18 @@
 angular.module('alurapic')
-.controller('FotosController', function($scope){
-	$scope.foto = {
-		url: 'http://lorempixel.com/260/360',
-		titulo: 'lorem'
-	};
+.controller('FotosController', function($scope, $http){
+	var urlFotos = "http://localhost:3000/v1/fotos";
+	$scope.fotos = [];
+	var promise = $http.get(urlFotos);
+	promise.then(function(retorno){
+		$scope.fotos = retorno.data;
+	}).catch(function(error){
+		console.log(error);
+	});
+
+	$http.get(urlFotos)
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	}).error(function(erro){
+		console.log(erro);
+	})
 });
